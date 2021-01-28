1981bd151e556a8230321aae4fc148141b20ec20
DanyaText <danille506@gmail.com>
2021-01-28 17:24:37 +0300

    add Window and Panel

--- src/com/company/Window.java
+++ src/com/company/Window.java
@@ -1,4 +1,12 @@
 package com.company;
 
-public class Window {
+import javax.swing.*;
+
+public class Window extends JFrame {
+    public Window(){
+setBounds(20,21,800,600);
+setResizable(false);
+setDefaultCloseOperation(EXIT_ON_CLOSE);
+getContentPane().add(new MyPanel());
+    }
 }
